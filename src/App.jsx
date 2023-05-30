1ac2a2e9d38c1582fbf0925b8f3e50f0ff41db5b
King Robert Kehinde <robert.k@techie.com>
2023-05-30 08:10:38 +0100

    Fetching Movie Review From NYTimes API

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Reviews from './Components/Reviews'
+
+function App() {
+
+  return (
+    <>
+      <Reviews/>  
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Reviews.jsx
@@ -0,0 +1,54 @@
+import { Component } from "react";
+import axios from "axios";
+
+class Reviews extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movies: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchMovies();
+
+    this.pollingInterval = setInterval(this.fetchMovies, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.pollingInterval);
+  }
+
+  fetchMovies = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=3AnqdqcAh9BPx7gOJCbX7qsohsT8hRjO"
+      );
+
+      const data = await response.data.results;
+
+      this.setState({ movies: data });
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    }
+  };
+
+  render() {
+    const { movies } = this.state;
+
+    return (
+      <div>
+        {movies.map((movie, index) => (
+          <div key={index}>
+            <p>Title: {movie.display_title}</p>
+            <p>Critics Pick: {movie.critics_pick}</p>
+            <p>Headline: {movie.headline}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Reviews;
